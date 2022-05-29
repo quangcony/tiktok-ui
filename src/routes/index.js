fbd8acc44c30de8e97169205d4be2012e3a5badd
Dang Quang <quangcony.01@gmail.com>
2022-05-29 18:35:02 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+// import layouts
+import HeaderOnly from '~/component/Layouts/HeaderOnly';
+
+// pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
